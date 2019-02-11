c65290da5c6fd09ba1df2134a93752e0fa14f403
Kosuke Abe <kousuke1201abe@gmail.com>
2019-02-11 18:04:47 +0900

    change migration file

--- db/migrate/20181016132347_create_questions.rb
+++ db/migrate/20181016132347_create_questions.rb
@@ -1,10 +1,13 @@
 class CreateQuestions < ActiveRecord::Migration[5.2]
-  def change
+  def up
     create_table :questions do |t|
       t.text :content
       t.integer :user_id
 
       t.timestamps
     end
+  end
+
+  def down
   end
 end
